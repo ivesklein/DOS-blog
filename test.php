f932802952459bb38c77cf6da2aec17115d25c16
ivesklein <ivesklein>
2014-10-22 00:58:18 -0400

    i hate firefox (it hasn't background-position-x/y)

--- test.php
+++ test.php
@@ -11,7 +11,7 @@ function c($n){
 	return "<i class='c".$n."'></i>";
 }
 
-function table($text){
+function table($type, $text, $align="center"){
 	
 	$return ="";
 	$return .= c("C9"); //esquina
@@ -22,38 +22,84 @@ function table($text){
 
 	$return .= c("BB");//esquina
 	
+		
+	$end=false;
+	$text2 = $text;
+	while($end==false){
+		if(strlen($text2)>78){
+			$cutted=false;
+			for($i=78;$i>0;$i--){
+				$char = substr( $text2, $i, 1 );
+				if($char==" "){
+					$line   = substr( $text2, 0, $i );
+					$text2  = substr( $text2, $i+1);
+					//echo($text2);
+					$cutted=true;
+					break;
+				}
+				//echo($text2);
+			}
+			if($cutted==false){
+				$line   = substr( $text2, 0, 78 );
+				$text2  = substr( $text2, 78+1);
+			}
 
-	$long = strlen($text);
 
-	//if($long<=78){
-	if(true){
+		}else{
+			$line=$text2;
+			$end=true;
+		}
+
 		$return .= c("BA");//borde izq
 
-		$resto = 78 - $long;
+		$resto = 78 - strlen($line);
 		$izq = floor($resto/2)*9;
 		$der = ceil($resto/2)*9;
-		
-		$return .= "<div class='word' style='width:".$izq."px'></div>";
-		$return .= "<div class='word'>".$text."</div>";
-		$return .= "<div class='word' style='width:".$der."px'></div>";
 
-		$return .= c("BA");//borde izq
-		$return .= c("C8");//esquina
-		for($i=1;$i<79;$i++){
-			$return .= c("CD");	 //	borde inf
+		if($align=="center"){$return .= "<div class='word' style='width:".$izq."px'></div>";}
+		if($align=="right"){$return .= "<div class='word' style='width:".($resto*9)."px'></div>";}
+		
+		if($type=="ubuntu"){
+			$strlen = strlen( $line );
+			for( $i = 0; $i <= $strlen; $i++ ) {
+			    $char = substr( $line, $i, 1 );
+			    if(ord($char)!=0){
+					$return .= c(strtoupper(dechex(ord($char))));
+				}
+			    // $char contains the current character, so do your processing here
+			}
+		}else{
+			$return .= "<div class='word'>".$line."</div>";
 		}
-		$return .= c("BC");//esquina
+
+		if($align=="center"){$return .= "<div class='word' style='width:".$der."px'></div>";}
+		if($align=="left"){$return .= "<div class='word' style='width:".($resto*9)."px'></div>";}
+
+		$return .= c("BA");//borde der
 
 	}
+
+	$return .= c("C8");//esquina
+	for($i=1;$i<79;$i++){
+		$return .= c("CD");	 //	borde inf
+	}
+	$return .= c("BC");//esquina
 
 	return $return;
 
 }
 
-echo table("Hola!! esto es un texto de largo 78 justo, para probar que pasa con la funcion");
 
-echo table("Hola!!! esto es un texto de largo 79 justo, para probar que pasa con la funcion");
+
+echo table("ubuntu", "Hola!! esto es un texto de largo 78 justo, para probar que pasa con la funcion");
+echo table("dos","Hola!! esto es un texto de largo 78 justo, para probar que pasa con la funcion");
+
+echo table("ubuntu","Hola!!! esto es un texto de largo 79 justo, para probar que pasa con la funcion", "right");
+echo table("dos","Hola!!! esto es un texto de largo 79 justo, para probar que pasa con la funcion", "left");
 
 ?></div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="css/dos.js"></script>
 </body>
 </html>
